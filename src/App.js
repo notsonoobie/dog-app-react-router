88d0a90ae95d3ffbcd65a55e7f90507f92b1f84e
Rahul Gupta <swastikmedical74@gmail.com>
2020-07-02 12:27:27 +0530

    Project Termination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Routes from './Routes';
+import Navbar from './Navbar'
+import Whisky from './images/whiskey.jpg';
+import Hazel from './images/hazel.jpg';
+import Tubby from './images/tubby.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  static defaultProps = {
+    dogs : [
+      {
+        name  : "Whisky",
+        age   : 5,
+        src   : Whisky,
+        facts : [
+          "Whisky loves eating popcorn",
+          "Whisky is a terrible guard dog",
+          "Whisky wants to cuddle with you!"
+        ]
+      },
+      {
+        name  : "Hazel",
+        age   : 5,
+        src   : Hazel,
+        facts : [
+          "Hazel loves eating popcorn",
+          "Hazel is a terrible guard dog",
+          "Hazel wants to cuddle with you!"
+        ]
+      },
+      {
+        name  : "Tubby",
+        age   : 5,
+        src   : Tubby,
+        facts : [
+          "Tubby loves eating popcorn",
+          "Tubby is a terrible guard dog",
+          "Tubby wants to cuddle with you!"
+        ]
+      }
+    ]
+  }
 
+  render(){
+    return (
+      <div>
+        <Navbar dogs={this.props.dogs} />
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
